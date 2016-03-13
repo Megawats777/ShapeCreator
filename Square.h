56a3c0591ec8313f2b85307b2c2bdca040fc727b
Megawats777 <jedsemilla@gmail.com>
2016-03-12 19:39:10 -0500

    Created Square class

---
+++ Square.h
@@ -0,0 +1,33 @@
+// Create a square
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Square
+{
+public:
+
+	// Initialize Object
+	Square();
+
+	// Deconstructor
+	~Square();
+
+	// Ask the user for the dimensions of the sqaure
+
+
+	// Create the square
+
+
+
+private:
+
+	// Shape Variables
+	int stars;
+	int lengths;
+
+	int exitNum;
+
+};
+
