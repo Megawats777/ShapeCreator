2bedb25391dcbd82cc34dc6230c4f4ef1f02c49c
Megawats777 <jedsemilla@gmail.com>
2016-06-17 23:52:52 -0400

    Added functionality to create stars

--- Star.h
+++ Star.h
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 class Star
 {
@@ -20,24 +21,39 @@ public:
 	// Create the star
 	void createStar();
 
+	// Request the star length
+	void requestStarLength();
+
 	// Print stars
-	void printStars();
+	void printStars(int rowNum);
 
 	// Print spaces
-	void printSpaces();
+	void printSpaces(int rowNum);
+
+	// Print a new line
+	void printNewLine();
+
+	// Set the amount of spaces for the next line
+	void setNewSpaceAmount(int rowNum);
+
+	// Set the amount of stars for the next line
+	void setNewStarAmount(int rowNum);
+
+	// The exit message
+	void exitMessage();
 
 private:
 
 	/*--The private properties of the class--*/
+
+	// int requested length
+	int requestedLength = 2;
 
 	// The amount of stars
 	int stars;
 
 	// The amount of spaces
 	int spaces;
-
-	// Is the star creation at the half way point
-	bool isCreationAtHalfWayPoint = false;
 
 	// The exit number
 	int exitNum = -1;
